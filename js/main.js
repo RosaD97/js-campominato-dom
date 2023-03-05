40ed8f13fefe8a6d9e8031b148b7028773095db8
RosaD97 <r960595@gmail.com>
2023-03-05 18:08:36 +0100

    creazione di bombe

--- js/main.js
+++ js/main.js
@@ -8,13 +8,23 @@ function creaCella(elemento, classe, classeDue, valore){
     elementoCreato.classList.add(classeDue);
     elementoCreato.innerText = valore;
 
-    elementoCreato.addEventListener('click', function(){
-        elementoCreato.classList.add('colors');
-        alert(valore);
-    })
-
+    elementoCreato.addEventListener('click', function(){    
+        if(!bombeGenerate.includes(valore)){
+            elementoCreato.classList.add('colors');
+            if(!score.includes(valore)){
+                score.push(valore);
+                count++;
+                punteggio.innerHTML = count;
+            }
+        } else {
+            elementoCreato.classList.add('colorsBomb');
+            console.log(bombeGenerate)
+            elementoCreato.removeEventListener('click', function() {});    
+        }
+    });
     return elementoCreato;
 }
+
 // Appendi Cella
 function appendiCella(container, element){
     container.append(element);
@@ -27,17 +37,20 @@ function start(){
     container.innerHTML = '';
 
     if(scelta === 'normal'){
+        bombeGenerate = generaBombe(15, 1, 81);
         for(let i = 1; i <= 81; i++){
             const cella = creaCella('div', 'cella', 'normal', i);
             appendiCella(container, cella);
         }
     } else if (scelta === 'hard'){
+        bombeGenerate = generaBombe(15, 1, 49);
         for(let i = 1; i <= 49; i++){
             const cella = creaCella('div', 'cella', 'hard', i);
             appendiCella(container, cella);
         }
     } else {
         for(let i = 1; i <= 100; i++){
+            bombeGenerate = generaBombe(15, 1, 100);
             const cella = creaCella('div', 'cella', 'easy', i);
             appendiCella(container, cella);
         }
@@ -47,27 +60,33 @@ function start(){
     container.classList.remove('d-none');
 }
 
-// Genera numeri casuali da 1 a 16
+// Genera numeri casuali da 1 a 100
 function numeriACaso(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+// Funzione genera bombe
+function generaBombe(num, min, max){
+    const nCasuali = [];
+    while(nCasuali.length <= num){
+        let generati = numeriACaso(min, max);
+        if(!nCasuali.includes(generati)){
+            nCasuali.push(generati);
+        }
+    }
+    return nCasuali;
 }
 
 // MAIN
 const container = document.querySelector(".ms_container");
 const button = document.querySelector(".ms_btn");
+const punteggio = document.querySelector(".punteggio");
+let count = 0;
 
-const nCasuali = [];
-const generati = numeriACaso(1, 6);
+let bombeGenerate = generaBombe();
+const score = [];
 
-for(let i = 0; i <= 16 - 1; i++){
-    if(generati != nCasuali[i]){
-    nCasuali.push(generati);
-    } else {
 
-    }
-}
-console.log(nCasuali)
-console.log(generati)
+
 
 
 
